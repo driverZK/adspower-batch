c25d05b5d636e26fc70fccc3b38ccf8abde3298e
driverZK <wisdom.geeker@gmail.com>
2025-07-18 14:17:16 +0800

    add read mail

---
+++ outlook/read_email.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+import requests
+
+# 配置 AdsPower WebDriver
+ads_power_webdriver_url = "http://local.adspower.net:50325/api/v2/browser-profile/start"
+
+def get_ads_browser(profile_id):
+    """
+    用API查询ads浏览器环境
+    """
+    param = {
+        "profile_id": str(profile_id),
+        "proxy_detection": "0",
+        "headless": "0"
+    }
+    res = requests.post(ads_power_webdriver_url, json=param)
+    ads_data = res.json()
+    print(ads_data)
+    sel = ads_data['data']['ws']['selenium']
+
+    return sel
+
+options = Options()
+
+profile_id = "k11smc25"
+debuggerAddress = get_ads_browser(profile_id)
+print(debuggerAddress)
+
+options.add_experimental_option("debuggerAddress", debuggerAddress)  # 从 API 返回的地址
+
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://outlook.live.com/mail/inbox")
+time.sleep(15)
+
+driver.quit()
