48ba287ce0268a4a7c13919d2b367241a7d6df57
driverZK <wisdom.geeker@gmail.com>
2025-07-25 17:26:07 +0800

    fix error

--- outlook/read_email.py
+++ outlook/read_email.py
@@ -12,7 +12,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException 
 import logging
 
 # 配置日志
